0022161c96affa1a379552e5211b0f874e8cc27a
Samuel Berton <samuel.berton@student.kuleuven.be>
2023-05-21 14:44:22 +0200

    Cleaned upt the animation

--- pages/visual2.js
+++ pages/visual2.js
@@ -8,11 +8,37 @@ import { RepeatWrapping, NearestFilter, DoubleSide } from "three";
 import css from "@/styles/Home.module.css";
 import Telemetry from "@/components/telemetry";
 import { useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from "three";
 
 function Drone() {
   const mesh = useRef();
+  const [model, setModel] = useState();
   let ind = useRef(0);
   let elapsedTime = useRef(0);
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load("/scene.gltf", (gltf) => {
+      const model = gltf.scene;
+
+      let box = new THREE.Box3().setFromObject(model);
+      let modelWidth = box.max.x - box.min.x;
+      let modelHeight = box.max.y - box.min.y;
+      let modelDepth = box.max.z - box.min.z;
+
+      let scaleX = 0.3 / modelWidth;
+      let scaleY = 0.3 / modelHeight;
+      let scaleZ = 0.3 / modelDepth;
+
+      let scale = Math.min(scaleX, scaleY, scaleZ);
+
+      model.scale.set(scale, scale, scale);
+
+      setModel(model);
+      setModel(gltf.scene);
+    });
+  }, []);
 
   useFrame((state, delta) => {
     elapsedTime.current += delta;
@@ -26,24 +52,20 @@ function Drone() {
       }
 
       elapsedTime.current = 0;
-      mesh.current.position.x = myJson.results[ind.current].drone.states[0];
-      mesh.current.position.y = myJson.results[ind.current].drone.states[1];
-      mesh.current.position.z = myJson.results[ind.current].drone.states[2];
+      if (mesh.current) {
+        mesh.current.position.x = myJson.results[ind.current].drone.states[0];
+        mesh.current.position.y = myJson.results[ind.current].drone.states[1];
+        mesh.current.position.z = myJson.results[ind.current].drone.states[2];
+      }
     }
   });
 
-  return (
-    <Sphere ref={mesh} args={[0.3, 64, 64]}>
-      <meshStandardMaterial color="#00ff83" />
-    </Sphere>
-  );
+  return model ? <primitive ref={mesh} object={model} /> : null;
 }
 
 function Ground() {
-  const texture = useTexture(
-    "https://threejs.org/manual/examples/resources/images/checker.png"
-  );
-  const planeSize = 40;
+  const texture = useTexture("/textures/grass.png");
+  const planeSize = 8000;
   const repeats = planeSize / 2;
 
   texture.wrapS = texture.wrapT = RepeatWrapping;
@@ -71,14 +93,13 @@ export default function Home() {
   return (
     <div className={css.scene}>
       <Canvas>
-        <color attach="background" args={["#000"]} />
+        <color attach="background" args={["#87ceeb"]} />
         <Suspense fallback={null}>
           <Drone />
           <Ground />
         </Suspense>
         <OrbitControls />
-        <ambientLight intensity={0.1} />
-        <pointLight intensity={1} position={[0, 10, 10]} distance={100} />
+        <ambientLight intensity={0.5} />
         <perspectiveCamera position={[0, 10, 20]} fov={45} />
       </Canvas>
     </div>
