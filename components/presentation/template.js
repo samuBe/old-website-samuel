4113ddc5fb890218820b9c9e3f4c18dabf4fa5c8
Samuel Berton <samuel.berton@student.kuleuven.be>
2023-05-24 12:14:46 +0200

    Added the title slide to the template

--- components/presentation/template.js
+++ components/presentation/template.js
@@ -17,10 +17,51 @@ const TitleSlide = () => {
         width={1}
         marginBottom={-16}
       >
-        <Box padding="8px 1em 0px" position={"absolute"} bottom={0} left={0}>
-          <FullScreen color={theme.colors.tertiary} />
+        <Box position={"relative"} left={40} marginTop={-40}>
+          <Image
+            src="/thesis/presentation/KUL.png"
+            width={260}
+            height={96}
+            alt="Logo KU Leuven"
+          ></Image>
+        </Box>
+        <Box
+          position={"absolute"}
+          right={0}
+          marginRight={-16}
+          bottom={0}
+          marginBottom={-16}
+          marginTop={-40}
+          zIndex={2}
+        >
+          <Image
+            src="/thesis/presentation/sedes.png"
+            width={517}
+            height={600}
+            alt="Logo KU Leuven"
+          ></Image>
         </Box>
       </Box>
+      <FlexBox
+        justifyContent="space-between"
+        position="absolute"
+        alignItems={"center"}
+        bottom={0}
+        width={1}
+        backgroundColor={theme.colors.secondary}
+        paddingTop={20}
+        paddingBottom={20}
+        marginBottom={-16}
+      >
+        <FlexBox left="15em" alignItems={"center"} justifyContent={"center"}>
+          <Box padding="8px 1em 0px">
+            <FullScreen color={theme.colors.tertiary} />
+          </Box>
+          <Box padding="0 1em">
+            <AnimatedProgress color={theme.colors.tertiary} />
+          </Box>
+        </FlexBox>
+      </FlexBox>
     </>
   );
 };
